40e627210a1bf9e765396dfdd511d596441b8870
Castro Hamiltone <castromiltone@gmail.com>
2023-11-27 22:35:55 +0300

    fix

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  return initialNumber + (expansion1989 !== undefined ? expansion1989 : 89) + (expansion2019 !== undefined ? expansion2019 : 19);
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
 }
