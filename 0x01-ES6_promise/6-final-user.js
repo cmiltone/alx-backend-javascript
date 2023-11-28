a78f3788313df8bc5989d3c34791798abfb6d11b
Castro Hamiltone <castromiltone@gmail.com>
2023-11-28 22:45:31 +0300

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((data) => data.map((item) => ({ status: item.status, value: item.value })));
+    .then((data) => data.map((item) => ({ status: item.status === 'fulfilled' ? item.status : String(item.reason), value: item.value })));
 }
