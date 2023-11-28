d550cc2008c2f3280a8ebaeb2e83715e8f0904db
Castro Hamiltone <castromiltone@gmail.com>
2023-11-28 21:55:19 +0300

    fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([createUser(), uploadPhoto()])
-    .then((data) => console.log(`${data[0].firstName} ${data[0].lastName}`))
+    .then((data) => console.log(`${data[1].body} ${data[0].firstName} ${data[0].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
