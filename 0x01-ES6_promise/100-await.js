d58c1ce6b67add5340f21b83b2a076ea8dd8b51f
Castro Hamiltone <castromiltone@gmail.com>
2023-11-28 23:29:12 +0300

    fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -5,6 +5,8 @@ export default async function asyncUploadUser() {
     const res1 = await uploadPhoto();
     const res2 = await createUser();
 
+    if (!res1 || !res2) return { photo: null, user: null };
+
     return {
       photo: res1.body,
       user: res2,
