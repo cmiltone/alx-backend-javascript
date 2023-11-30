851563ada93d397268935daaab7ca4e8104358a8
Castro Hamiltone <castromiltone@gmail.com>
2023-11-30 22:44:12 +0300

    100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-underscore-dangle */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(val) {
+    this._range = val;
+  }
+
+  cloneCar() {
+    const Clone = super.constructor[Symbol.species];
+
+    return new Clone();
+  }
+}
