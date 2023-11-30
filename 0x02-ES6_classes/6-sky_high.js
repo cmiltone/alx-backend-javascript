bc24c022913d4d5c37c28b59734be843fb6d1646
Castro Hamiltone <castromiltone@gmail.com>
2023-11-30 21:45:25 +0300

    6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(val) {
+    if (typeof val !== 'number') throw new TypeError('Floors must be a number');
+    else this._floors = val;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
