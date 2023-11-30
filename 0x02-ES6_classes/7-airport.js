a351f6558a020385e4d2f7c9f3c6371afae82e2d
Castro Hamiltone <castromiltone@gmail.com>
2023-11-30 21:50:22 +0300

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('Name must be a string');
+    else this._name = val;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (typeof val !== 'string') throw new TypeError('Code must be a string');
+    else this._code = val;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
