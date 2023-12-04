77e44909a587b9790db91425c33bb3de21fe4982
Castro Hamiltone <castromiltone@gmail.com>
2023-12-04 21:01:35 +0300

    100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,9 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.get(endpoint)) weakMap.set(endpoint, 0);
+
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+}
