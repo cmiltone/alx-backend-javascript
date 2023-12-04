d894d8bd8aa20b253b2a3af5853f2d211deef932
Castro Hamiltone <castromiltone@gmail.com>
2023-12-04 21:09:03 +0300

    fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (!weakMap.get(endpoint)) weakMap.set(endpoint, 0);
+  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
 
   if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 
