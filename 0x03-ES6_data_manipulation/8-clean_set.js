ea464cd2a059b27f530fe5a019ef98e3664db8e9
Castro Hamiltone <castromiltone@gmail.com>
2023-12-04 20:09:26 +0300

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,5 +2,5 @@ export default function cleanSet(set, str) {
   if (!str) return '';
   const arr = Array.from(set);
 
-  return arr.filter((item) => item.includes(str)).map((item) => item.replace(str, '')).join('-');
+  return arr.filter((item) => typeof item === 'string' && item.includes(str)).map((item) => item.replace(str, '')).join('-');
 }
